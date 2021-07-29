aee84e795af0e2f7055a522c0087bbd7cf18792b
nicorive94 <nicodanielrivero94@hotmail.com>
2021-07-29 20:33:58 -0300

    commit

--- app.js
+++ app.js
@@ -5,21 +5,17 @@ const app = express();
 //coneccion a base de datos
 const mongoose = require('mongoose');
 
-//
 require('dotenv').config()
-module.exports = async () =>{
-    await mongoose.connect(process.env.DB_URL,
+
+ mongoose.connect(process.env.DB_URL,
         {useNewUrlParser: true,
         useUnifiedTopology: true}
     )
         .then(() => console.log("base de datos conectada"))
         .catch(e => console.log(e))
-}
+
 
 const port = process.env.PORT || 3000;
-
-
-
 
 // const user = "nicorive";
 // const password ="lasheras88";
@@ -27,10 +23,8 @@ const port = process.env.PORT || 3000;
 
 // const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.f0gix.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
-
-
-
 app.set('view engine', 'ejs');
+
 app.set('views',__dirname + '/vistas')
 
 app.use(express.static(__dirname + "/public"))
