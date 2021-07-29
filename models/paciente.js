db4d6be15dd62dbbf44394e370f0ea632d19e69b
nicorive94 <nicodanielrivero94@hotmail.com>
2021-07-29 15:15:11 -0300

    lpm

--- models/paciente.js
+++ models/paciente.js
@@ -9,6 +9,6 @@ const pacienteSchema = new Schema({
 
 // crear modelo
 
-const Paciente = mongoose.model('paciente', pacienteSchema);
+const Paciente = mongoose.model('Paciente', pacienteSchema);
 
 module.exports = Paciente;
--- router/pacientes.js
+++ router/pacientes.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const Paciente = require('../models/paciente');
+// const Paciente = require('../models/paciente');
 
 
-router.get('/prueba', async (req, res) =>{
+// router.get('/prueba', async (req, res) =>{
 
-    try {
-        const arrayPacientesDB = await Paciente.find()
-        console.log(arrayPacientesDB)
-    } catch (error) {
-        console.log(error)
-    }
+//     try {
+//         const arrayPacientesDB = await Paciente.find()
+//         console.log(arrayPacientesDB)
+//     } catch (error) {
+//         console.log(error)
+//     }
 
 
-    res.render("prueba", {pacientes: [
-        {numero:"1", nombre: "Nicolás", apellido:"Rivero", fecha:"09/02/1994"},
-        {numero:"2", nombre: "Sofia", apellido:"Correa", fecha: "19/01/1994"}
-    ]})
-})
+//     res.render("prueba", {pacientes: [
+//         {numero:"1", nombre: "Nicolás", apellido:"Rivero", fecha:"09/02/1994"},
+//         {numero:"2", nombre: "Sofia", apellido:"Correa", fecha: "19/01/1994"}
+//     ]})
+// })
 
-module.export = router;
+// module.export = router;
